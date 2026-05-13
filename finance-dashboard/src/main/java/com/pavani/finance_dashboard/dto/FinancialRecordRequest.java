4fa766d4d11ec4eb3cb24cfcea4a53030f45af56
sunkaraa-pavanii <sunkaraa.pavanii@gmail.com>
2026-05-13 13:07:10 +0530

    Update FinancialRecordRequest.java

--- finance-dashboard/src/main/java/com/pavani/finance_dashboard/dto/FinancialRecordRequest.java
+++ finance-dashboard/src/main/java/com/pavani/finance_dashboard/dto/FinancialRecordRequest.java
@@ -4,12 +4,12 @@ import lombok.Data;
 import java.time.LocalDate;
 
 @Data
-public class FinancialRecordRequest {
+private class FinancialRecordRequest {
 
-    private Double amount;
-    private String type; // INCOME / EXPENSE
-    private String category;
-    private LocalDate date;
-    private String notes;
-    private Long userId;
+    public Double amount;
+    public String type; // INCOME / EXPENSE
+    public String category;
+    public LocalDate date;
+    public String notes;
+    public Long userId;
 }
